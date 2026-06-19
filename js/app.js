68a644722ea360227f46aac9f75fcc1f7b7b3c7a
Ajeet yadav <ajeetyadavajju88@gmail.com>
2026-06-19 07:13:06 +0530

    feat: create Thinkora sidebar header with logo and title

--- js/app.js
+++ js/app.js
@@ -4,3 +4,31 @@ const body = document.body;
 const container = document.createElement("div");
 container.classList.add("container");
 body.appendChild(container);
+
+// left-container
+const leftContainer = document.createElement("div");
+leftContainer.classList.add("left-container");
+container.appendChild(leftContainer);
+
+// left-container- top-div
+const topDiv = document.createElement("div");
+topDiv.classList.add("top-div");
+leftContainer.appendChild(topDiv)
+
+// top-div-content
+const logoDiv = document.createElement("div");
+logoDiv.classList.add("logo-div");
+topDiv.appendChild(logoDiv);
+
+// logo
+const logo = document.createElement("img");
+logo.classList.add("logo");
+logo.src = "icons/logo.jpg";
+logo.alt = "Thinkora Logo";
+logoDiv.appendChild(logo);
+
+// heading
+const heading = document.createElement("h1");
+heading.classList.add("heading")
+heading.textContent = "Thinkora";
+topDiv.appendChild(heading);
